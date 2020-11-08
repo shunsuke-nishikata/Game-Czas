02c7542333d4a04ea0062755ce729f1c21694d38
EC2 Default User <ec2-user@ip-172-31-7-194.ec2.internal>
2020-11-08 14:34:58 +0000

    ユーザー詳細ページ、編集ページ作成#7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,38 @@
 class UsersController < ApplicationController
+  
+  before_action :ensure_correct_user, only: [:edit, :update]
+  before_action :set_user, only: [:show, :edit, :update]
+  
   def index
   end
   
   def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    @user.update(user_params)
+    redirect_to user_path(current_user.id)
+  end
+  
+  private
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    if current_user.id != @user.id
+      redirect_back(fallback_location: root_path)
+    end
+  end
+  
+  
+  def user_params
+    params.require(:user).permit(:image,:name,:sex,:age,:favorite_game,:introduction,:twitter,:instagram,:youtube)
   end
 end
