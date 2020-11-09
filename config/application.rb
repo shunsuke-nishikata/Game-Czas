db1a811d53d2e9e34839dc71a5e10675ab0ea0e0
EC2 Default User <ec2-user@ip-172-31-7-194.ec2.internal>
2020-11-09 06:35:53 +0000

    カレンダー実装#9

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,21 @@ module GameCzas
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    # 日本時間に設定
+    # https://qiita.com/shimadama/items/7e5c3d75c9a9f51abdd5を参照
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    
+    # デフォルトを日本語に設定
+    config.i18n.default_locale = :ja
+    # 複数のロケールファイルが読み込まれるようにpathを通す記述
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    
+    
+    # 週の始まりの曜日の設定を日曜日に変える→カレンダーの曜日のスタートを日曜日に
+    # https://railsdoc.com/page/config_beginning_of_weekを参照
+    config.beginning_of_week = :sunday
   end
 end
